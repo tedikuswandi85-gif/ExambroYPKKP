180341dccc87435911c691f2037d22f2f28ff7d9
tedikuswandi85-gif <tedikuswandi85@admin.smp.belajar.id>
2026-04-28 08:55:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const databaseUjian = [
     { tingkat: "9", mapel: "PABP", link: "https://bit.ly/PSAJPAIBP2526", tgl: "2026-05-07", durasi: 90, token: "ALLAHBANTUSAYALULUS" },
     { tingkat: "9", mapel: "Pend. Pancasila", link: "https://bit.ly/ASASPPKN9-2526", tgl: "2026-05-04", durasi: 90, token: "PP9" },
     { tingkat: "9", mapel: "Bahasa Indonesia", link: "https://bit.ly/ASAS1-BI92526", tgl: "2026-05-06", durasi: 90, token: "BI9" },
-    { tingkat: "9", mapel: "Bahasa Inggris", link: "https://bit.ly/SAS9ENGLISH", tgl: "2026-05-05", durasi: 120, token: "ENG9" },
+    { tingkat: "9", mapel: "Bahasa Inggris", link: "https://bit.ly/PSAJ_English2526", tgl: "2026-05-05", durasi: 120, token: "eng2526" },
     { tingkat: "9", mapel: "IPA", link: "https://forms.gle/Fm26nSSpXEvFERaAA", tgl: "2026-0405-05", durasi: 90, token: "IPAUS26" },
     { tingkat: "9", mapel: "IPS", link: "https://bit.ly/PSAJ_IPS_9_2026", tgl: "2026-05-06", durasi: 90, token: "MinimalTauSejarah" },
     { tingkat: "9", mapel: "Matematika", link: "https://docs.google.com/forms/d/e/1FAIpQLScUL0P-yN3jQQHbhmmPfn-s9zJeywKM4d-jCbVi2d851IGYlg/viewform?usp=header", tgl: "2026-05-04", durasi: 90, token: "SMPYPKKP2026" },
